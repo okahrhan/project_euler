9ff39d8de5fd6fbe2fbe9b5533d068fa014e309a
o_gzhan_cakir <oguzhancakir24@gmail.com>
2025-09-28 20:12:24 +0300

    ok

--- Names_Scores/Name_Scores.py
+++ Names_Scores/Name_Scores.py
@@ -1,36 +1,19 @@
+def counter(name: str, idx: int) -> int:
+    dic = {chr(i + 64): i for i in range(1, 27)}
+    return_value = sum(dic[ch] for ch in name) * (idx + 1)
+    return return_value
+
+
 def names_score():
-    A = 1
-    B = 2
-    C = 3
-    D = 4
-    E = 5
-    F = 6
-    G = 7
-    H = 8
-    I = 9
-    J = 10
-    K = 11
-    L = 12
-    M = 13
-    N = 14
-    O = 15
-    P = 16
-    Q = 17
-    R = 18
-    S = 19
-    T = 20
-    U = 21
-    V = 22
-    W = 23
-    X = 24
-    Y = 25
-    Z = 26
-    # names.txt dosyasını oku
-    with open('sirali_isimler.txt', 'r') as file:
-        # Her satırı oku
-        names = file.readlines()
+    with open("sorted_names.txt", "r") as file:
+        names = file.read().replace('"', "").split(",")
+
+    result_value = 0
+
+    for i in range(len(names)):
+        result_value += counter(names[i], i)
+
+    return result_value
 
 
-    for name in names:
-        print(name.strip())
-names_score()
+print(names_score())
